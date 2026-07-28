638336e622fd1b00f5b0edd439983d7941cf7433
Ivan Litvak <vanya.litvak@gmail.com>
2026-07-28 20:45:07 +0300

    feat: add batchnorm output backward gate

--- native/cuda/mgt_cuda/mlp_batch_norm_forward.cuh
+++ native/cuda/mgt_cuda/mlp_batch_norm_forward.cuh
@@ -6,4 +6,5 @@ namespace mgt_cuda {
 struct NcclRankContext;
 std::uint64_t MlpBatchNormForwardWorkspaceFloats(const CudaMlpShape& physical_shape,const mgt::BatchNormTrainingPlan& batch_norm,std::uint32_t local_rows);
 mgt::Status LaunchMlpBatchNormForward(const CudaMlpShape& physical_shape,std::uint32_t logical_hd1,std::uint32_t logical_hd2,const float* weights,float* batch_norm_affine,float* batch_norm_running,const mgt::TrainStateStorage* states,std::uint32_t local_rows,std::uint32_t global_rows,float* outputs,float* workspace,std::uint64_t workspace_floats,const mgt::BatchNormTrainingPlan& plan,NcclRankContext* context,cublasHandle_t blas,cudaStream_t stream);
+mgt::Status LaunchMlpBatchNormOutputLossGrad(const CudaMlpShape& physical_shape,const float* weights,const float* labels,const float* outputs,const float* final_activation,std::uint32_t local_rows,std::uint32_t global_rows,float* loss,float* weight_grad,float* output_dy,float* final_activation_grad,NcclRankContext* context,cublasHandle_t blas,cudaStream_t stream);
 }
