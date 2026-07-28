3a85b4977150c2127cd237e191f0722aa09bec0c
Ivan Litvak <vanya.litvak@gmail.com>
2026-07-28 18:37:14 +0300

    feat: add global nccl sync batchnorm

---
+++ native/cuda/sync_batch_norm.cuh
@@ -0,0 +1,8 @@
+#pragma once
+#include "mgt/static_contracts.hpp"
+#include <cuda_runtime.h>
+#include <nccl.h>
+namespace mgt_cuda {
+mgt::Status LaunchSyncBatchNormForward(const float* x, int local_rows, int global_rows, int cols, const float* gamma, const float* beta, float* running_mean, float* running_var, float momentum, float epsilon, float* y, float* mean, float* inv_std, float* normalized, float* stats_workspace, ncclComm_t comm, cudaStream_t stream);
+mgt::Status LaunchSyncBatchNormBackward(const float* dy, int local_rows, int global_rows, int cols, const float* gamma, const float* inv_std, const float* normalized, float* dx, float* dgamma, float* dbeta, float* stats_workspace, ncclComm_t comm, cudaStream_t stream);
+}
